34ddf110841a4785b8e361265efc2fe1d64a3da4
Ryan Hill <ryanhill1128@gmail.com>
2020-12-16 19:21:04 -0600

    Added counter to page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  const decreaseCount = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  }
+
+  const increaseCount = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+
   return ( 
     <div>
-      <h1>Hello from AWS Amplify</h1>
+      <div className="page-title">
+        <h1>Hello from AWS Amplify</h1>
+      </div>
+      <div className="counter">
+        <p>Counter: {count}</p>
+        <button onClick={() => decreaseCount()}>-</button>
+        <button onClick={() => increaseCount()}>+</button>
+      </div>
     </div>
   );
 }
